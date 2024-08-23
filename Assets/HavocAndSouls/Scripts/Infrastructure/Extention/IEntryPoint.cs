1551c0f6360b7cb95a5788be03c97e88476ae02d
Hard_Code <zero-0_0@bk.ru>
2024-08-23 15:37:16 +0500

    Added Extention for Unity

---
+++ Assets/HavocAndSouls/Scripts/Infrastructure/Extention/IEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using System.Collections;
+
+namespace HavocAndSouls
+{
+    public interface IEntryPoint
+    {
+        IEnumerator Intialization(DIContainer parentContainer);
+    }
+}
